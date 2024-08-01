86ca1d17a6f588dde315ba57bec3d8b62b1338dc
younes <lassiriyounesse04@gmail.com>
2024-08-01 21:05:42 +0100

    'footer-responsive'

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -5,21 +5,27 @@ import fidalgodark from '../sushi_project_images/footer_bg-dark.png';
 function Footer(props) {
     return(
         <div className={props.dark? 'footer-section-dark' : 'footer-section'} id={props.menu? 'footer-section-menu' : null}>
-            <div className='footer-section-one'>
-                <span>PINTEREST</span>
-                <div className="footer-square"></div>
-                <span>FACEBOOK</span>
-                <div className="footer-square"></div>
-                <span>INSTAGRAM</span>
-            </div>
-            <div className='footer-section-two'>
-                <img src={props.dark? fidalgodark : fidalgo} alt='fidalgo'/>
-            </div>
-            <div className='footer-section-three'>
-                <span>© 2024 YOUNES LASSIRI, ALL RIGHTS RESERVED</span>
+            <div className='footer-section-left-side'></div>
+            <div className='footer-section-center-side'><img src={props.dark? fidalgodark : fidalgo} alt='fidalgo'/></div>
+            <div className='footer-section-right-side'></div>
+            <div className='footer-section-bottom'>
+                <div className='footer-section-bottom-left'>
+                    <span>PINTEREST</span>
+                    <div className="footer-square"></div>
+                    <span>FACEBOOK</span>
+                    <div className="footer-square"></div>
+                    <span>INSTAGRAM</span>
+                </div>
+                <div className='footer-section-bottom-right'>
+                    © 2024 YOUNES LASSIRI, ALL RIGHTS RESERVED
+                </div>
             </div>
         </div>
     )
 }
 
 export default Footer;
+
+
+
+            
